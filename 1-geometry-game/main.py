fe53ae536e5e6316a4952f5ccfdc22983e7b351b
DENN87 <denis.schiopu@yahoo.com>
2022-04-12 19:53:48 -0700

    Development Steps for Geometry Game

---
+++ 1-geometry-game/main.py
@@ -0,0 +1,13 @@
+"""
+Development Steps for Geometry Game
+
+    1. Writing down the objects on paper
+            point, rectangle
+    2. Writing a class for each object
+            class Point, class Rectangle
+    3. Writing methods for each class
+
+    4. Calling the classes and their methods
+
+"""
+
