8f3a8053141fd09f50084aa5edcd6ef701c21432
DENN87 <denis.schiopu@yahoo.com>
2022-04-13 16:14:18 -0700

    Implemented the generate_pdf method.

--- 2-Flatmates-Bill/main.py
+++ 2-Flatmates-Bill/main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as
@@ -26,7 +29,8 @@ class HouseMate:
         return f"{self.name} was in the house for {self.days_in_house} days."
 
     def pays(self, bill, renter_2):
-        return (self.days_in_house / (self.days_in_house + renter_2.days_in_house)) * bill.amount
+        return round(self.days_in_house / (self.days_in_house + renter_2.days_in_house) * bill.amount, 2)
+
 
 class PdfReport:
     """
@@ -38,12 +42,40 @@ class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
+    def generate_pdf(self, renter_1, renter_2, bill):
+        pdf = FPDF(orientation='P', unit='pt', format='letter')
+        pdf.add_page()
 
-current_bill = Bill(800, "March 2022")
+        # Insert title
+        pdf.set_font(family='Helvetica', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Housemates Bill", border=1, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family='Helvetica', size=14, style='B')
+        pdf.cell(w=100, h=30, txt="Period", border=1, align="C")
+        pdf.cell(w=150, h=30, txt=bill.period, border=1, align="C", ln=1)
+
+        # Insert Renters
+        pdf.set_font(family='Helvetica', size=12, style='I')
+        pdf.cell(w=100, h=20, txt=renter_1.name, border=1, align="C")
+        pdf.cell(w=150, h=20, txt=str(renter_1.pays(bill, renter_2)), border=1, align="C", ln=1)
+
+        pdf.cell(w=100, h=20, txt=renter_2.name, border=1, align="C")
+        pdf.cell(w=150, h=20, txt=str(renter_2.pays(bill, renter_1)), border=1, align="C")
+
+        # Write to file
+        pdf.output(self.filename)
+
+
+current_bill = Bill(700, "March 2022")
 print(current_bill)
 
-bob = HouseMate("Bob", 15)
-anne = HouseMate("Anne", 25)
+bob = HouseMate("Bob", 21)
+anne = HouseMate("Anne", 27)
 
 print(f"{bob} Sharing ${bob.pays(current_bill, anne)} of the total bill.")
 print(f"{anne} Sharing ${anne.pays(current_bill, bob)} of the total bill.")
+
+
+pdf_report = PdfReport("Report1.pdf")
+pdf_report.generate_pdf(bob, anne, current_bill)
