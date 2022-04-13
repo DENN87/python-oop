8aafafb6696dd19d036bccd25a732ca3d3e57b06
DENN87 <denis.schiopu@yahoo.com>
2022-04-13 14:52:36 -0700

    Created class HouseMate, constructor method.

--- 2-Flatmates-Bill/main.py
+++ 2-Flatmates-Bill/main.py
@@ -11,3 +11,16 @@ class Bill:
 bill = Bill(3000, "March 2022")
 print(bill)
 
+
+class HouseMate:
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def __repr__(self):
+        return f"{self.name} was in the house for {self.days_in_house} days."
+
+
+mate_1 = HouseMate("Bob", 15)
+print(mate_1)
