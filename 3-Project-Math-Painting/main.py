200c54bb88fee56460bb951a1fe941c0a8b12606
DENN87 <denis.schiopu@yahoo.com>
2022-04-13 23:15:37 -0700

    Implemented class Rectangle

--- 3-Project-Math-Painting/main.py
+++ 3-Project-Math-Painting/main.py
@@ -13,7 +13,7 @@ class Square:
 
 
 class Rectangle:
-
+    """A rectangle shape that can be drawn on a Canvas object"""
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
@@ -22,7 +22,9 @@ class Rectangle:
         self.color = color
 
     def draw(self, canvas):
-        pass
+        """Draws itself into the canvas"""
+        # Changes a slice of the array with new values
+        canvas.data[self.x: self.x + self.height, self.y: self.y + self.width] = self.color
 
 
 class Canvas:
