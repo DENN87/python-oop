d047a42718fccf33d7a7bd829f701ff6078d75ef
DENN87 <denis.schiopu@yahoo.com>
2022-04-14 13:05:26 -0700

    Implemented the Open Link button

--- 4-Webcam-Photo-Sharer/main.py
+++ 4-Webcam-Photo-Sharer/main.py
@@ -1,4 +1,5 @@
 import time
+import webbrowser
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -43,22 +44,35 @@ class FileSharer:
 
 
 class ImageScreen(Screen):
-    """
-    Accesses the photo file_path, uploads it to the filestack.com
-    and inserts the link in the Label widget
-    """
+
+    link_message = "Create a link first!"
 
     def create_link(self):
+        """
+        Accesses the photo file_path, uploads it to the filestack.com
+        and inserts the link in the Label widget
+        """
+
         _file_path = App.get_running_app().root.ids.camera_screen.file_path
         file_to_share = FileSharer(_file_path)
         self.url = file_to_share.share()
         self.ids.img_link.text = self.url
 
     def copy_link(self):
+        """Copy link to the clipboard for pasting"""
+
         try:
             Clipboard.copy(self.url)
         except:
-            self.ids.img_link.text = "Create a link first."
+            self.ids.img_link.text = self.link_message
+
+    def open_link(self):
+        """Open link in the browser"""
+
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.img_link.text = self.link_message
 
 
 class RootWidget(ScreenManager):
