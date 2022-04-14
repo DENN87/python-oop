701e0e1ccc23ffe8735321815fb4baf825fe570f
DENN87 <denis.schiopu@yahoo.com>
2022-04-14 11:38:17 -0700

    Initialized kivy library and it's classes

--- 4-Webcam-Photo-Sharer/main.py
+++ 4-Webcam-Photo-Sharer/main.py
@@ -1,3 +1,11 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+
+Builder.load_file('frontend.kv')
+
+
 class Webcam:
 
     def start(self):
@@ -19,3 +27,19 @@ class FileSharer:
     def share(self):
         pass
 
+
+class ImageScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
