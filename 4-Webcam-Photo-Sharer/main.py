386878f7f16dffac450ca73c4c3d85cdf0eeee41
DENN87 <denis.schiopu@yahoo.com>
2022-04-14 13:09:09 -0700

    Added comments

--- 4-Webcam-Photo-Sharer/main.py
+++ 4-Webcam-Photo-Sharer/main.py
@@ -12,18 +12,24 @@ Builder.load_file('frontend.kv')
 
 
 class CameraScreen(Screen):
-
     def start(self):
+        """Starts camera and changes Button text"""
+
         self.ids.camera.play = True
         self.ids.btn_start_stop.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
+        """Stops camera and changes Button text"""
+
         self.ids.camera.play = False
         self.ids.btn_start_stop.text = 'Start Camera'
         self.ids.camera.texture = None
 
     def capture(self):
+        """Creates a filename with current time and captures and saves
+        a image under that filename and path"""
+
         self.file_path = f"files/{time.strftime('%Y%m%d-%H%M%S')}.png"
         self.ids.camera.export_to_png(self.file_path)
         self.manager.current = 'image_screen'
@@ -31,13 +37,16 @@ class CameraScreen(Screen):
 
 
 class FileSharer:
-
     # Get api_key from www.filestack.com
+
     def __init__(self, filepath, api_key="AffkdoQtLQeqyulzBexkVz"):
         self.filepath = filepath
         self.api_key = api_key
 
     def share(self):
+        """Uploads the filename from file_path to the filestack.com
+        and returns back the url of the image"""
+
         client = Client(self.api_key)
         new_file_link = client.upload(filepath=self.filepath)
         return new_file_link.url
