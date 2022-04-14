930d9c1af9f687495bde47c61e1dbf00e6efc836
DENN87 <denis.schiopu@yahoo.com>
2022-04-14 15:49:02 -0700

    Implemented Web Forms with Flask WTForms

--- 5-House-Renters-Bill-Web-App/app.py
+++ 5-House-Renters-Bill-Web-App/app.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from wtforms import Form
+from wtforms import Form, StringField, SubmitField
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,7 +13,9 @@ class HomePage(MethodView):
 class BillFormPage(MethodView):
 
     def get(self):
-        return render_template('bill_form.html')
+        bill_form = BillForm()
+        return render_template('bill_form.html',
+                               bill_form=bill_form)
 
 
 class ResultsPage(MethodView):
@@ -23,8 +25,19 @@ class ResultsPage(MethodView):
 
 
 class BillForm(Form):
-    pass
+    amount = StringField("Bill Amount: ")
+    period = StringField("Bill Period: ")
 
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in the house: ")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in the house: ")
+
+    name3 = StringField("Name: ")
+    days_in_house3 = StringField("Days in the house: ")
+
+    button = SubmitField("Calculate")
 
 app.add_url_rule('/', view_func=HomePage.as_view('index'))
 app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form'))
