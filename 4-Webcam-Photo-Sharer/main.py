7887f7e677649f79ab3278db844c16ae6a3ed4eb
DENN87 <denis.schiopu@yahoo.com>
2022-04-14 11:33:12 -0700

    Created empty classes

---
+++ 4-Webcam-Photo-Sharer/main.py
@@ -0,0 +1,21 @@
+class Webcam:
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+
+class FileSharer:
+
+    def __init__(self, filepath, api_key):
+        self.filepath = filepath
+        self.api = api_key
+
+    def share(self):
+        pass
+
