670c5abacb47eb270e9ecc80fa874e5002a5f6c3
DENN87 <denis.schiopu@yahoo.com>
2022-04-14 15:33:17 -0700

    Created basic templates for views

--- 5-House-Renters-Bill-Web-App/app.py
+++ 5-House-Renters-Bill-Web-App/app.py
@@ -1,20 +1,33 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 class HomePage(MethodView):
-    pass
+
+    def get(self):
+        return render_template('index.html')
 
 
 class BillFormPage(MethodView):
-    pass
+
+    def get(self):
+        return render_template('bill_form.html')
 
 
 class ResultsPage(MethodView):
-    pass
+
+    def get(self):
+        return render_template('results.html')
 
 
 class BillForm(Form):
     pass
 
 
+app.add_url_rule('/', view_func=HomePage.as_view('index'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form'))
+app.add_url_rule('/results', view_func=ResultsPage.as_view('results'))
+
+app.run()
