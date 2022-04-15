18739d8a35b570c52a12665e899d7d3bfd9df4da
DENN87 <denis.schiopu@yahoo.com>
2022-04-15 00:40:08 -0700

    Created and initialized Empty classes

--- 7-Automated-Emails/main.py
+++ 7-Automated-Emails/main.py
@@ -1,3 +1,15 @@
 import requests
 from pprint import pprint
 
+class NewsFeed:
+    """
+    Representing multiple news titles and links as a single string
+    """
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
+
+    def get(self):
+        pass
