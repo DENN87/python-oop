487cfae1d20830bd271d2a8dc790138a5861b19e
DENN87 <denis.schiopu@yahoo.com>
2022-04-15 00:51:00 -0700

    Implemented the Get Data from API

--- 7-Automated-Emails/main.py
+++ 7-Automated-Emails/main.py
@@ -5,6 +5,10 @@ class NewsFeed:
     """
     Representing multiple news titles and links as a single string
     """
+
+    base_url = "http://newsapi.org/v2/everything"
+    api_key = "890603a55bfa47048e4490069ebee18c"
+
     def __init__(self, interest, from_date, to_date, language):
         self.interest = interest
         self.from_date = from_date
@@ -12,4 +16,20 @@ class NewsFeed:
         self.language = language
 
     def get(self):
-        pass
+        url = f"{self.base_url}?qInTitle={self.interest}" \
+              f"&from={self.from_date}&to={self.to_date}" \
+              f"&language={self.language}&apiKey={self.api_key}"
+
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+
+        email_body = ''
+        for article in articles:
+            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
+
+        return email_body
+
+
+news_feed = NewsFeed('tesla', '2022-03-14', '2022-04-14', 'en')
+print(news_feed.get())
