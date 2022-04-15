9dc281b2023c84c42fed05222ff8ef4176e4d781
DENN87 <denis.schiopu@yahoo.com>
2022-04-15 00:10:56 -0700

    Implemented Calorie page UI and Form data.

--- 6-Project-Calorie-Webapp/main.py
+++ 6-Project-Calorie-Webapp/main.py
@@ -18,17 +18,39 @@ class HomePage(MethodView):
 class CaloriesFormPage(MethodView):
 
     def get(self):
-        pass
+        calories_form = CaloriesForm()
+
+        return render_template('calories_form_page.html',
+                               calories_form=calories_form)
 
     def post(self):
-        pass
+        calories_form = CaloriesForm(request.form)
+
+        temp = Temperature(calories_form.country.data, calories_form.city.data)
+
+        calorie = Calorie(
+            float(calories_form.weight.data),
+            float(calories_form.height.data),
+            float(calories_form.age.data),
+            temp.get()
+        )
+
+        return render_template('calories_form_page.html',
+                               calories_form=calories_form,
+                               calories=calorie.calculate(),
+                               result=True)
 
 
 class CaloriesForm(Form):
-    pass
+    weight = StringField("Weight (kg):")
+    height = StringField("Height (cm):")
+    age = StringField("Age:")
+    country = StringField("Country:", default="USA")
+    city = StringField("City:", default="San Francisco")
+    button = SubmitField("Calculate My Calories")
 
 
-app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/', view_func=HomePage.as_view('index'))
 app.add_url_rule('/calories-form', view_func=CaloriesFormPage.as_view('calories_form_page'))
 
 if __name__ == "__main__":
