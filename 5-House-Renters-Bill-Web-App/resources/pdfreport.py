f36cee8366fafa27091e51e665180bdae6223016
DENN87 <denis.schiopu@yahoo.com>
2022-04-14 18:34:28 -0700

    Added fileshare upload to web

--- 5-House-Renters-Bill-Web-App/resources/pdfreport.py
+++ 5-House-Renters-Bill-Web-App/resources/pdfreport.py
@@ -76,3 +76,23 @@ class PdfReport:
 
         # Write to file
         pdf.output(self.filename)
+
+
+from filestack import Client
+
+
+class FileShare:
+    # Get api_key from www.filestack.com and added to api_key="<key>"
+
+    def __init__(self, filepath, api_key="<your_key>"):
+        self.filepath = filepath
+        self.api_key = api_key
+
+    def share(self):
+        """Uploads the filename from file_path to the filestack.com
+        and returns the url of the image"""
+
+        client = Client(self.api_key)
+        new_file_link = client.upload(filepath=self.filepath)
+        return new_file_link.url
+
