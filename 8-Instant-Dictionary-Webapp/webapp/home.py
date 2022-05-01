11b6193f1e89ebd391c96508739f52db733252e5
DENN87 <denis.schiopu@yahoo.com>
2022-04-30 22:17:04 -0700

    Created the Menu items for Home

--- 8-Instant-Dictionary-Webapp/webapp/home.py
+++ 8-Instant-Dictionary-Webapp/webapp/home.py
@@ -12,6 +12,17 @@ class Home:
         toolbar = jp.QToolbar(a=header)
 
         drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left", bordered=True)
+
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
+        jp.A(a=qlist, text="Home", href="/home", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="About", href="/about", classes=a_classes)
+        jp.Br(a=qlist)
+
         jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu", click=cls.move_drawer, drawer=drawer)
 
         jp.QToolbarTitle(a=toolbar, text="Quick and Easy English Dictionary")
@@ -19,6 +30,7 @@ class Home:
         container = jp.QPageContainer(a=layout)
 
         div = jp.Div(a=container, classes="bg-gray-200 h-screen")
+
         jp.Div(a=div, text="This is the Home page!", classes="text-4xl p-2")
         jp.Div(a=div, text="""
                     Home ipsum crackalackin black amet, sizzle adipiscing ass. Bizzle we gonna chung velizzle, pimpin' volutpizzle, suscipit brizzle, the bizzle vizzle, arcu. Pellentesque egizzle tortor. Sed erizzle. Fusce izzle pizzle dapibizzle turpizzle tempizzle for sure. Maurizzle pellentesque nibh bow wow wow izzle. Sizzle izzle tortor. 
