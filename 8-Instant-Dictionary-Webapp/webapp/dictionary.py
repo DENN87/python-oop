4e9bf64d389b6dc1b281e4152851ef80653df957
DENN87 <denis.schiopu@yahoo.com>
2022-04-30 19:07:59 -0700

    Getting Output instantly on typing

--- 8-Instant-Dictionary-Webapp/webapp/dictionary.py
+++ 8-Instant-Dictionary-Webapp/webapp/dictionary.py
@@ -13,24 +13,26 @@ class Dictionary:
     def serve(cls):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-        jp.Div(a=div, text="Quick and Easy English Dictionary", classes="text-4xl m-2")
-        jp.Div(a=div, text="Get the definition of any English word instantly as you type.", classes="text-m mx-2")
+        jp.Div(a=div, text="Quick and Easy English Dictionary", classes="text-4xl m-2 text-center p-5")
+        jp.Div(a=div, text="Get the definition of any English word instantly as you type.",
+               classes="text-m mx-2 text-center")
 
-        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        input_div = jp.Div(a=div, classes="grid grid-cols-1 p-5")
+
+        output_div = jp.Div(a=div, classes="justify-items-stretch m-2 p-2 text-lg border-2 h-40 ")
         input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
+                             outputdiv=output_div,
                              classes="m-2 bg-gray-100 border-2 border-gray-200 "
                                      "rounded focus:bg-white "
                                      "focus:outline-none "
-                                     "focus:border-gray-500 py-2 px-4")
+                                     "focus:border-gray-500 py-2 px-4 w-48 justify-self-center")
 
-        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
-        jp.Button(a=input_div, text="Get Definition", classes="border-2",
-                  click=cls._get_definition, outputdiv=output_div, inputbox=input_box)
+        input_box.on('input', cls._get_definition)
 
         return wp
 
     @staticmethod  # simply behaves like a function, independent
     def _get_definition(widget, msg):
-        defined = definition.Definition(widget.inputbox.value).get()
+        defined = definition.Definition(widget.value).get()
         widget.outputdiv.text = " ".join(defined)
 
