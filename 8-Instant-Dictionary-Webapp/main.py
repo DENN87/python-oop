85a13a0b0848d921affc6358575d3cdd49b2467f
DENN87 <denis.schiopu@yahoo.com>
2022-04-30 18:00:13 -0700

    Created Dictionary page

--- 8-Instant-Dictionary-Webapp/main.py
+++ 8-Instant-Dictionary-Webapp/main.py
@@ -1,9 +1,11 @@
 import justpy as jp
 
+from webapp.dictionary import Dictionary
 from webapp.home import Home
 from webapp.about import About
 
 
+jp.Route(Dictionary.path, Dictionary.serve)
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
 
