6c85ea4f2a6d27931631718dc7076a5f9b26c7b6
DENN87 <denis.schiopu@yahoo.com>
2022-04-30 18:28:22 -0700

    Getting Output onclick in the Output Div

--- 8-Instant-Dictionary-Webapp/webapp/dictionary.py
+++ 8-Instant-Dictionary-Webapp/webapp/dictionary.py
@@ -4,15 +4,26 @@ import justpy as jp
 class Dictionary:
     path = "/dictionary"
 
-    def serve(self):
+    @classmethod
+    def serve(cls):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen w-100")
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="Quick and Easy English Dictionary", classes="text-4xl m-2")
         jp.Div(a=div, text="Get the definition of any English word instantly as you type.", classes="text-m mx-2")
 
-        jp.Input(a=div, placeholder="Type in a word here...", classes="m-2 bg-gray-100 border-2 border-gray-200 "
-                                                                      "rounded w-80 focus:bg-white focus:outline-none "
-                                                                      "focus:border-gray-500 py-2 px-4")
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        jp.Input(a=input_div, placeholder="Type in a word here...",
+                 classes="m-2 bg-gray-100 border-2 border-gray-200 "
+                         "rounded focus:bg-white "
+                         "focus:outline-none "
+                         "focus:border-gray-500 py-2 px-4")
+
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        jp.Button(a=input_div, text="Get Definition", classes="border-2", click=cls._get_definition, outputdiv=output_div)
 
         return wp
+
+    @staticmethod  # simply behaves like a function, independent
+    def _get_definition(widget, msg):
+        widget.outputdiv.text = "You see this now !"
+
