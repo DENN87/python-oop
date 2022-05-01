a1d6d8ce3fc6b1dde106476d9aa6bc41ccef1ab2
DENN87 <denis.schiopu@yahoo.com>
2022-04-30 18:44:59 -0700

    Getting the definitions in the Output box

--- 8-Instant-Dictionary-Webapp/webapp/dictionary.py
+++ 8-Instant-Dictionary-Webapp/webapp/dictionary.py
@@ -1,4 +1,9 @@
 import justpy as jp
+import sys
+from pathlib import Path
+sys.path.append(str(Path('.').absolute().parent))
+
+import definition
 
 
 class Dictionary:
@@ -12,18 +17,20 @@ class Dictionary:
         jp.Div(a=div, text="Get the definition of any English word instantly as you type.", classes="text-m mx-2")
 
         input_div = jp.Div(a=div, classes="grid grid-cols-2")
-        jp.Input(a=input_div, placeholder="Type in a word here...",
-                 classes="m-2 bg-gray-100 border-2 border-gray-200 "
-                         "rounded focus:bg-white "
-                         "focus:outline-none "
-                         "focus:border-gray-500 py-2 px-4")
+        input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 "
+                                     "rounded focus:bg-white "
+                                     "focus:outline-none "
+                                     "focus:border-gray-500 py-2 px-4")
 
         output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
-        jp.Button(a=input_div, text="Get Definition", classes="border-2", click=cls._get_definition, outputdiv=output_div)
+        jp.Button(a=input_div, text="Get Definition", classes="border-2",
+                  click=cls._get_definition, outputdiv=output_div, inputbox=input_box)
 
         return wp
 
     @staticmethod  # simply behaves like a function, independent
     def _get_definition(widget, msg):
-        widget.outputdiv.text = "You see this now !"
+        defined = definition.Definition(widget.inputbox.value).get()
+        widget.outputdiv.text = " ".join(defined)
 
